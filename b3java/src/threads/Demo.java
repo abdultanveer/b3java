0627b5ab37cd285a7bcb13d2e6b18c165e03e1c3
abdul ansari <abdul.tanveer@gmail.com>
2022-09-09 19:46:56 +0530

    join

--- b3java/src/threads/Demo.java
+++ b3java/src/threads/Demo.java
@@ -1,11 +1,23 @@
 package threads;
 
 public class Demo {
-public static void main(String[] args) {
-	Accountant accountant1 = new Accountant();
-	Accountant accountant2 = new Accountant();
-	
-	accountant1.run();
-	accountant2.run();
-}
+	public static void main(String[] args) throws InterruptedException {
+		/*
+		 * Accountant accountant1 = new Accountant(); Accountant accountant2 = new
+		 * Accountant();
+		 * 
+		 * accountant1.run(); accountant2.run();
+		 */
+		
+		Joyce joyce = new Joyce();
+		Virkin virkin = new Virkin();
+		Zach zach = new Zach();
+		
+		virkin.start();
+		zach.start();
+		
+		zach.join();//waiting for zach to complete and join us for lunch
+		
+		joyce.start();
+	}
 }
